605a00784c6f3d4e80eb2071203ef5d683c5e9b1
Kevin Loughead <kvnloughead@gmail.com>
2020-04-05 20:53:03 -0400

    Basically functionality for BEM Nested file structure in place.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Assumes user defined css within ./blocks directory.
 
 import shutil, os, sys
 
+# TODO Create root directory 
 def make_all_directories(blocks):
     """
     Calls on make_directories to walk through dictionary of BEM blocks
@@ -16,21 +17,29 @@ def make_all_directories(blocks):
     underscores, but include all underscores for modifiers (including leading) 
     """
     for block in blocks:
-        # make block level directories
-        path = os.path.join('blocks', block)
-        os.mkdir(path)
-        # make element level directories
+        # make block level directories and css files
+        make_directories(block)
+        make_css_files(block, block)
         for element in blocks[block]:
+            # make element level directories
             if "_" not in element:
                 make_directories(block, f"__{element}")
+                make_css_files(f"{block}__{element}", block, f"__{element}")
             elif element.startswith("_"):
                 # make block_modifier directories
-                mod = element.split("_")[1]
+                try:
+                    elem, mod, val = element.split("_")
+                    filename = f"{block}_{mod}_{val}"
+                except ValueError:
+                    elem, mod = element.split("_")
+                    filename = f"{block}_{mod}"
                 make_directories(block, f"_{mod}")
+                make_css_files(filename, block, f"_{mod}")
             else: 
                 # make block__element_modifier directories
                 elem, mod, val = element.split("_")
                 make_directories(block, f"__{elem}", f"_{mod}")
+                make_css_files(f"{block}__{elem}_{mod}_{val}", block, f"__{elem}", f"_{mod}")
 
 def make_directories(*path_parts):
     """Forms path blocks/path_parts and makes corresponding directory."""
@@ -40,7 +49,17 @@ def make_directories(*path_parts):
     except FileExistsError:
         pass
 
-blocks = {
+def make_css_files(filename, *path_parts):
+    # make css files
+    filename = f"{filename}.css"
+    path = os.path.join("blocks", *path_parts, filename)
+    print(path)
+    with open(path, "w+") as f:
+        selector = f".{filename}"[:-4]
+        f.write(selector + " {\n\n}")
+
+
+blocks = {  'root'   : [],
             'header' : ['logo', 'title', 'subtitle', '_theme_dark'],
             'footer' : ['logo', 'copyright', 'links'],
             'content': ['main-text', 'sidebar', 'sidebar_place_left', 'sidebar_place_right'],   
